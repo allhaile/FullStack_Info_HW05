5346862218b610127984eb7e5c4401d979f10082
Haile Shavers <hshavers@gmail.com>
2018-03-01 15:14:33 -0800

    Adding final files

--- app/views.py
+++ app/views.py
@@ -8,18 +8,26 @@ app.secret_key = os.environ.get('SECRET_KEY') or 'hard to guess string'
 @app.route('/index')
 def index():
     username = ''
-    if (): #check if the user is already in session, if so, direct the user to survey.html Hint: render_template with a variable
-        pass
+    if 'username' in session:
+        username = session['username']
+        return render_template('survey.html', name=username)
     else:
         return render_template('login.html')
 
-@app.route('/login') # You need to specify something here for the function to get requests
+    # if (): #check if the user is already in session, if so, direct the user to survey.html Hint: render_template with a variable
+
+@app.route('/login', methods =['GET','POST']) # You need to specify something here for the function to get requests
 def login():
     # Here, you need to have logic like if there's a post request method, store the username and email from the form into
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        username = session['username']
+
+        session['email'] = request.form['email']
+        return render_template('survey.html', name=username)
+    else:
     # session dictionary
-    if():
-        pass
-    return None
+        return render_template('index.html')
 
 @app.route('/logout')
 def logout():
@@ -31,13 +39,16 @@ def logout():
 def submitSurvey():
     username = ''
     email = ''
-    if(): #check if user in session
+    if 'username' in session: #check if user in session
         username = session.get('username')
         surveyResponse = {}
-        #get the rest o responses from users using request library Hint: ~3 lines of code
+        surveyResponse['food'] = request.form.get('food')
+        surveyResponse['color'] = request.form.get('color')
+        surveyResponse['vacation'] = request.form.get('vacation')
+
         surveyResponse['fe-before'] = request.form.get('feBefore')
         surveyResponse['fe-after'] = request.form.get('feAfter')
-        return render_template('results.html') # pass in variables to the template
+        return render_template('results.html', surveyResponse=surveyResponse, name=username) # pass in variables to the template
     else:
         return render_template('login.html')
 
